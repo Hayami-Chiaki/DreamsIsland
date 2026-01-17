cd176072db5e45c7b3555e7d261c85b727494d1c
xiaotiancai820 <2048653907@qq.com>
2026-01-17 14:59:24 +0800

    更新广场模块ui

--- app/src/main/java/com/example/dreamisland/MainActivity.java
+++ app/src/main/java/com/example/dreamisland/MainActivity.java
@@ -46,9 +46,6 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
-        // 禁用图标着色
-        navView.setItemIconTintList(null);
-
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
